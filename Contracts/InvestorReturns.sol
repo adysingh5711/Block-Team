2e8f1e7e535e2e2e6ec1869beaa23825e9672869
Aditya Singh <singhaditya5711@gmail.com>
2024-04-14 12:12:17 +0530

    Added Images

--- Contracts/InvestorReturns.sol
+++ Contracts/InvestorReturns.sol
@@ -9,21 +9,36 @@ contract InvestmentReturn {
     event FundsAllocatedUpdated(uint amount);
 
     constructor(uint _initialPoolAmount) {
-        require(_initialPoolAmount > 0, "Initial pool amount must be greater than zero");
+        require(
+            _initialPoolAmount > 0,
+            "Initial pool amount must be greater than zero"
+        );
         investmentPoolAmount = _initialPoolAmount;
         emit InvestmentPoolInitialized(_initialPoolAmount);
     }
 
     function updateFundsAllocated(uint _newFundsAllocated) public {
-        require(_newFundsAllocated > 0, "Funds allocated must be greater than zero");
+        require(
+            _newFundsAllocated > 0,
+            "Funds allocated must be greater than zero"
+        );
         fundsAllocated = _newFundsAllocated;
         emit FundsAllocatedUpdated(_newFundsAllocated);
     }
 
-    function returnOnInvestment(uint investorContribution) public view returns (uint) {
-        require(investmentPoolAmount > 0, "Investment pool amount must be greater than zero");
-        uint contributionPercentage = (investorContribution * 100) / investmentPoolAmount;
-        require(contributionPercentage > 0, "Contribution percentage can't be zero");
+    function returnOnInvestment(
+        uint investorContribution
+    ) public view returns (uint) {
+        require(
+            investmentPoolAmount > 0,
+            "Investment pool amount must be greater than zero"
+        );
+        uint contributionPercentage = (investorContribution * 100) /
+            investmentPoolAmount;
+        require(
+            contributionPercentage > 0,
+            "Contribution percentage can't be zero"
+        );
 
         // Utilizing Hyperlane for gas optimization
         uint claimTokensWorth;
@@ -31,7 +46,9 @@ contract InvestmentReturn {
             // Hyperlane invocation
             claimTokensWorth := calldataload(0x04)
             returndatacopy(0x00, 0x00, returndatasize())
-            if iszero(call(gas(), 0x05, 0x00, 0x00, returndatasize(), 0x00, 0x00)) {
+            if iszero(
+                call(gas(), 0x05, 0x00, 0x00, returndatasize(), 0x00, 0x00)
+            ) {
                 revert(0x00, returndatasize())
             }
         }
